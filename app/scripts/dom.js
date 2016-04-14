575ed5e70c29c22479d0310c3af244bdc3c7e863
Henry Thach <thachhao@gmail.com>
2016-04-13 20:46:34 -0400

    Add testing for createPlateHtml(weight).

--- app/scripts/dom.js
+++ app/scripts/dom.js
@@ -3,11 +3,26 @@ const app = app || {};
 (function () {
   'use strict';
 
+  const PLATES = [45, 35, 25, 10, 5, 2.5];
+
   app.dom = {
     clearChildrenFrom(element) {
       while (element.firstChild) {
         element.removeChild(element.firstChild);
       }
+    },
+
+    getPlateWidthInPercentage(plate) {
+      var number = PLATES.indexOf(plate);
+      return 100 - (number * 10);
+    },
+
+    createPlateHtml(weight) {
+      var li = document.createElement('li');
+      li.className = 'plate';
+      li.innerHTML = weight;
+      li.style.width = app.dom.getPlateWidthInPercentage(weight) + '%';
+      return li;
     }
   };
 })();
--- test/spec/dom.js
+++ test/spec/dom.js
@@ -16,5 +16,37 @@
       dom.clearChildrenFrom(list);
       assert.equal('', list.innerHTML);
     });
+
+    it('should create a <li> element for a plate', () => {
+      var fourtyFive = dom.createPlateHtml(45);
+      assert.equal('plate', fourtyFive.className);
+      assert.equal('100%', fourtyFive.style.width);
+      assert.equal('45', fourtyFive.innerHTML);
+
+      var thirtyFive = dom.createPlateHtml(35);
+      assert.equal('plate', thirtyFive.className);
+      assert.equal('90%', thirtyFive.style.width);
+      assert.equal('35', thirtyFive.innerHTML);
+
+      var twentyFive = dom.createPlateHtml(25);
+      assert.equal('plate', twentyFive.className);
+      assert.equal('80%', twentyFive.style.width);
+      assert.equal('25', twentyFive.innerHTML);
+
+      var ten = dom.createPlateHtml(10);
+      assert.equal('plate', ten.className);
+      assert.equal('70%', ten.style.width);
+      assert.equal('10', ten.innerHTML);
+
+      var five = dom.createPlateHtml(5);
+      assert.equal('plate', five.className);
+      assert.equal('60%', five.style.width);
+      assert.equal('5', five.innerHTML);
+
+      var twoAndHalf = dom.createPlateHtml(2.5);
+      assert.equal('plate', twoAndHalf.className);
+      assert.equal('50%', twoAndHalf.style.width);
+      assert.equal('2.5', twoAndHalf.innerHTML);
+    });
   });
 })();
